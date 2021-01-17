47ce3a0070c98abfa3acf8db45de6bc759466cab
Thomas Webel <Thomaswebel@gmail.com>
2021-01-16 20:44:18 -0500

    fixed rounding error for all test cases

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -10,7 +10,7 @@ public class MakeChange {
 		double price = 0.0, paid = 0.0, owedBack = 0.0;
 
 		int $twentyD = 0, $tenD = 0, $fiveD = 0, $oneD = 0, $quarter = 0, $dime = 0, $nickle = 0,
-				$penny = 0;
+				$penny = 0, owedBackInt = 0;
 		
 		boolean hasPaid = false;
 
@@ -26,52 +26,57 @@ public class MakeChange {
 
 		}
 
-		owedBack = paid - price;
+		owedBack = (paid - price) + 0.001;
+		System.out.println(owedBack);
 
 		if (owedBack == 0) {
 			System.out.println("Thanks for exact change!");
 		}
 		System.out.printf("I owe you: $%.2f\n", owedBack);
+		
+		
 		owedBack *= 100;
-		owedBack = (int)(owedBack);
-		System.out.println(owedBack);
-		if (owedBack > 2000) {
-			$twentyD = (int) (owedBack / 2000);
-			owedBack %= 2000;
+//		System.out.println(owedBack);
+		owedBackInt = (int)(owedBack);
+//		System.out.println(owedBackInt);
+		
+		if (owedBackInt > 2000) {
+			$twentyD =  owedBackInt / 2000;
+			owedBackInt %= 2000;
 		}
-		if (owedBack >= 1000) {
-			$tenD = (int) (owedBack / 1000);
-			owedBack %= 1000;
+		if (owedBackInt >= 1000) {
+			$tenD = owedBackInt / 1000;
+			owedBackInt %= 1000;
 		}
-		if (owedBack >= 500) {
-			$fiveD = (int) (owedBack / 500);
-			owedBack %= 500;
+		if (owedBackInt >= 500) {
+			$fiveD = owedBackInt / 500;
+			owedBackInt %= 500;
 		}
-		if (owedBack >= 100) {
-			$oneD = (int) (owedBack / 100);
-			owedBack %= 100;
+		if (owedBackInt >= 100) {
+			$oneD = owedBackInt / 100;
+			owedBackInt %= 100;
 		}
-		if (owedBack >= 25) {
-			$quarter = (int) (owedBack / 25);
-			owedBack %= 25;
+		if (owedBackInt >= 25) {
+			$quarter = owedBackInt / 25;
+			owedBackInt %= 25;
 		}
-		if (owedBack >= 10) {
-			$dime = (int) (owedBack / 10);
-			owedBack %= 10;
+		if (owedBackInt >= 10) {
+			$dime = owedBackInt / 10;
+			owedBackInt %= 10;
 		}
-		if (owedBack >= 5) {
-			$nickle = (int) (owedBack / 5);
-			owedBack %= 5;
+		if (owedBackInt >= 5) {
+			$nickle = owedBackInt / 5;
+			owedBackInt %= 5;
 		}
-		if (owedBack >= 1) {
- 			$penny = (int) (owedBack / 1);
-			owedBack %= 1;
+		if (owedBackInt >= 1) {
+ 			$penny = owedBackInt / 1;
+			owedBackInt %= 1;
 		}
 //		Easy Check of every vars at end:
 		System.out.println("cost: " + price + ", paid: " + paid);
-		System.out.println("20s: " + $twentyD + ", 10s: " + $tenD + ", 5s: " + $fiveD + ", 1s: " + $oneD
-				+ ",\n quarters: " + $quarter + ", dimes: " + $dime + ", nickles: " + $nickle + ", penny: " + $penny);
-		System.out.println(owedBack);
+		System.out.println("twenties: " + $twentyD + ", tens: " + $tenD + ", fives: " + $fiveD + ", ones: " + $oneD
+				+ ",\nquarters: " + $quarter + ", dimes: " + $dime + ", nickles: " + $nickle + ", penny: " + $penny);
+		System.out.println(owedBackInt);
 		kb.close();
 	}
 
@@ -89,10 +94,37 @@ public class MakeChange {
 }
 
 /**
- * Amount: .67, Tendered: .50, Result: Error message Amount: .67, Tendered:
- * 1.00, Result: 1 quarter, 1 nickel, 3 pennies. Amount: .59, Tendered: 1.00,
- * Result: 1 quarter, 1 dime, 1 nickel, 1 penny. Amount: 3.96, Tendered: 20.00,
+ * Amount: .67, Tendered: .50,
+ * Result: Error message 
+ * 
+ * Amount: .67, Tendered: 1.00, 
+ * Result: 1 quarter, 1 nickel, 3 pennies. 
+ * 
+ * Amount: .59, Tendered: 1.00,
+ * Result: 1 quarter, 1 dime, 1 nickel, 1 penny. 
+ * 
+ * Amount: 3.96, Tendered: 20.00,
  * Result: 1 ten dollar bill, 1 five dollar bill, 1 one dollar bill, 4 pennies.
+ * 
  * Amount: any amount less than 20.00, Tendered: anything greater than amount:
+ * 
  * correct denominations for correct change.
  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
